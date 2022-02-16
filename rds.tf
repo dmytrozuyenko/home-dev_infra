fb12a2766bf80759c3853c25ffd0148ba4dc26d0
dmytrozuyenko <95592566+dmytrozuyenko@users.noreply.github.com>
2022-02-16 15:26:12 +0200

    Create rds.tf

---
+++ rds.tf
@@ -0,0 +1,18 @@
+##################
+# RDS Postgresql #
+##################
+
+resource "aws_db_instance" "postgres" {
+  identifier             = "postgres"
+  endpoint               = "postgres:5432"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "13.1"
+  username               = "postgres"
+  password               = var.db_password
+  db_subnet_group_name   = aws_vpc.home.id
+  vpc_security_group_ids = [aws_security_group.home.id]
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+}
