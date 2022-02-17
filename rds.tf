06b0acecad65e82e1647b9342ed9892292050065
dmytrozuyenko <95592566+dmytrozuyenko@users.noreply.github.com>
2022-02-17 15:27:55 +0200

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,41 +1,41 @@
-resource "aws_db_subnet_group" "db" {
-  subnet_ids = aws_subnet.private.id
+# resource "aws_db_subnet_group" "db" {
+#   subnet_ids = aws_subnet.private.id
   
-#   depends_on = [
-#     aws_subnet.private.id,
-#   ]
-}
+# #   depends_on = [
+# #     aws_subnet.private.id,
+# #   ]
+# }
 
 
-resource "aws_security_group" "db" {
-  name        = "db-security-group"
-  vpc_id      = aws_vpc.home.id
+# resource "aws_security_group" "db" {
+#   name        = "db-security-group"
+#   vpc_id      = aws_vpc.home.id
 
-  ingress {
-    protocol    = "tcp"
-    from_port   = 5432
-    to_port     = 5432
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     protocol    = "tcp"
+#     from_port   = 5432
+#     to_port     = 5432
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-resource "aws_db_instance" "postgres" {
-  identifier             = "postgres"
-  instance_class         = "db.t3.micro"
-  allocated_storage      = 5
-  engine                 = "postgres"
-  engine_version         = "13.1"
-  username               = "postgres"
-  password               = var.db_password
-  db_subnet_group_name   = aws_db_subnet_group.db.name
-  vpc_security_group_ids = [aws_security_group.db.id]
-  publicly_accessible    = true
-  skip_final_snapshot    = true
-}
+# resource "aws_db_instance" "postgres" {
+#   identifier             = "postgres"
+#   instance_class         = "db.t3.micro"
+#   allocated_storage      = 5
+#   engine                 = "postgres"
+#   engine_version         = "13.1"
+#   username               = "postgres"
+#   password               = var.db_password
+#   db_subnet_group_name   = aws_db_subnet_group.db.name
+#   vpc_security_group_ids = [aws_security_group.db.id]
+#   publicly_accessible    = true
+#   skip_final_snapshot    = true
+# }
